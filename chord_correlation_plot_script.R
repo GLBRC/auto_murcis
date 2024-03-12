ed9c710251e56076caa9bf559d06e6a11369bff3
kmyers <kmyers2@wisc.edu>
2024-03-12 14:38:07 -0500

    updating to use file for gene colors

--- chord_correlation_plot_script.R
+++ chord_correlation_plot_script.R
@@ -2,7 +2,9 @@
 
 # Script to print Chord diagrams and Correlation plots for Nicole NIH Project
 # Called as part of murcs_script.py.
-# Reqires the user to imput the directory containing the output files from the count_NIH_spacers.py script
+# Requires the user to input the directory containing the output files from the count_NIH_spacers.py script
+# Requires the user to input a headerless, two column, comma deliminted file with the first column
+#   being the gene names and the second column the colors for the chord diagram
 # Will look for files ending in the following:
 #   spacer_combinations_withoutReplacement_value_for_Chord_Diagrams_forPlotting.txt
 #   spacer_combinations_withReplacement_value_for_correlation_plots_forPlotting.txt
@@ -17,18 +19,29 @@ library(ggplot2)
 # process command line argument
 args <- commandArgs(TRUE)
 working.dir <- args[1]
+file <- args[2]
 setwd(working.dir)
-# set colors
-grid.col = c(lpg0059="aquamarine",lpg0086="bisque",lpg0107="blue",lpb1658="brown",
-             lpg0171="brown1",lpg0246="burlywood4",lpg0404="cadetblue",lpg0439="cadetblue1",
-             lpg0518="chartreuse",lpg0716="chartreuse4",lpg1099="chocolate",lpg1525="chocolate4",
-             lpg1527="coral",lpg1621="coral4",lpg1661="cyan",lpg1701="cyan3",lpg1907="darkgoldenrod",
-             lpg2223="purple",lpg2244="green",lpg2552="green4",lpg2584="honeydew",
-             lpg2692="hotpink",lpg2874="indianred1",lpg2884="khaki1",lpg2888="red",
-             lpg0016="lightblue1",lpg0096="lightgoldenrod3",lpg1959="lightpink",lpg2271="lightsalmon",
-             lpg2804="lightskyblue",lpg2885="magenta",lpg3000="blue",lpg1689="mediumorchid3",
-             lpg1776="mediumpurple1",lpg2628="olivedrab2",lpg0963="orange",lpg1137="orangered",
-             lpg0621="palegreen1",lpg2733="royalblue1",lpg2806="seagreen1",lpg1702="purple1")
+# set colors - DEPRECIATED
+# grid.col = c(lpg0059="aquamarine",lpg0086="bisque",lpg0107="blue",lpb1658="brown",
+#              lpg0171="brown1",lpg0246="burlywood4",lpg0404="cadetblue",lpg0439="cadetblue1",
+#              lpg0518="chartreuse",lpg0716="chartreuse4",lpg1099="chocolate",lpg1525="chocolate4",
+#              lpg1527="coral",lpg1621="coral4",lpg1661="cyan",lpg1701="cyan3",lpg1907="darkgoldenrod",
+#              lpg2223="purple",lpg2244="green",lpg2552="green4",lpg2584="honeydew",
+#              lpg2692="hotpink",lpg2874="indianred1",lpg2884="khaki1",lpg2888="red",
+#              lpg0016="lightblue1",lpg0096="lightgoldenrod3",lpg1959="lightpink",lpg2271="lightsalmon",
+#              lpg2804="lightskyblue",lpg2885="magenta",lpg3000="blue",lpg1689="mediumorchid3",
+#              lpg1776="mediumpurple1",lpg2628="olivedrab2",lpg0963="orange",lpg1137="orangered",
+#              lpg0621="palegreen1",lpg2733="royalblue1",lpg2806="seagreen1",lpg1702="purple1")
+
+# import name_color_file and set vector for colors to use
+name_color_file <- read.csv(file, header = F)
+
+# split file by columns into two vectors
+nameVec <- unlist(name_color_file[[1]], use.names = F)
+colorVec <- unlist(name_color_file[[2]], use.names = F)
+
+# make a named vector for gene labes and colors
+grid.col <- setNames(colorVec, nameVec)
 
 #Plot chord plots using files from murcs_script.py
 chord_files <- list.files(path = working.dir, pattern = "spacer_combinations_withoutReplacement_value_for_Chord_Diagrams_forPlotting.txt")
@@ -58,7 +71,7 @@ for(i in correlation_files){
   sample_name <- sub("_spacer_combinations_withReplacement_value_for_correlation_plots_forPlotting.txt", "", i)
   fileName = paste(sample_name,"pairwise_allHits_correlationPlot.pdf", sep = "_")
   fileName_minHit5 = paste(sample_name, "pairwise_minHit5_correlationPlot.pdf", sep = "_")
-
+  
   list1 <- read.table(file = i, sep = "\t", header = TRUE) #needs to have all the possible combinations
   
   #gradient chart additional 0-5 bin
@@ -69,10 +82,10 @@ for(i in correlation_files){
     geom_tile(aes(fill=group), color = "black") +
     coord_fixed() +
     scale_fill_manual(breaks = levels(list1_new$group), values = c("white", "wheat", "#BB9D00",
-    "#F8766D", "#E76BF3", "#00C0B8", "#00A5FF", "red2", "blue2", "green3", "purple4", "black"),
-    name = "Pairwise\nOccurances", labels = c("No Hit", "1-4", "5-10", "11-100", "101-500",
-    "501-1000", "1001-5000", "5001-10000", "10001-50000", "50001-100000", "100001-500000", 
-    "Same Spacer")) +
+                                                                   "#F8766D", "#E76BF3", "#00C0B8", "#00A5FF", "red2", "blue2", "green3", "purple4", "black"),
+                      name = "Pairwise\nOccurances", labels = c("No Hit", "1-4", "5-10", "11-100", "101-500",
+                                                                "501-1000", "1001-5000", "5001-10000", "10001-50000", "50001-100000", "100001-500000", 
+                                                                "Same Spacer")) +
     theme(axis.text.x = element_text(
       angle = 90)
     ) + 
@@ -89,11 +102,11 @@ for(i in correlation_files){
   plt <- ggplot(list1_new, aes(x=Spacer_1, y=Spacer_2)) +
     geom_tile(aes(fill=group), color = "black") +
     coord_fixed() +
-      scale_fill_manual(breaks = levels(list1_new$group), values = c("white", "#BB9D00", 
-      "#F8766D", "#E76BF3", "#00C0B8", "#00A5FF", "red2", "blue2", "green3", "purple4",
-      "black"), name = "Pairwise\nOccurances", labels = c("0-4", "5-10", "11-100", 
-      "101-500", "501-1000", "1001-5000", "5001-10000", "10001-50000", "50001-100000",
-      "100001-500000", "Same Spacer")) +
+    scale_fill_manual(breaks = levels(list1_new$group), values = c("white", "#BB9D00", 
+                                                                   "#F8766D", "#E76BF3", "#00C0B8", "#00A5FF", "red2", "blue2", "green3", "purple4",
+                                                                   "black"), name = "Pairwise\nOccurances", labels = c("0-4", "5-10", "11-100", 
+                                                                                                                       "101-500", "501-1000", "1001-5000", "5001-10000", "10001-50000", "50001-100000",
+                                                                                                                       "100001-500000", "Same Spacer")) +
     theme(axis.text.x = element_text(
       angle = 90)
     ) + 
