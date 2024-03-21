d46701265f7ff1d612167222a1c8ee5afb325caa
Michael Place <mplace@wisc.edu>
2024-03-21 09:58:02 -0500

    remove files

--- run_murcs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /opt/bifxapps/miniconda3/etc/profile.d/conda.sh
-unset $PYTHONPATH
-conda activate /home/glbrc.org/user/.conda/envs/murcis
-/home/glbrc.org/user/scripts/auto_murcis/murcs_script.py -f bamfiles -t gene_spaces_sequence-Nicole.txt
-conda deactivate
